8289f5904c35bc92e1c249dd9c0fbfac25492801
MarioSantoro <mariosantoro124@gmail.com>
2023-05-22 15:36:53 +0200

    Check palindrom string complete

---
+++ script.js
@@ -0,0 +1,16 @@
+function checkPalindromString(words) { 
+    let len = words.length;
+    
+    for (let i = 0; i < 1; i++) {  
+        if (words[i] !== words[len - 1 - i]) {  
+           console.log("La parola inserita non è palindroma");
+        }else{
+            console.log("La parola inserita è palindroma");
+        }
+    }
+}  
+
+
+let str = prompt("Inserisci una parola che sia palindroma");
+
+checkPalindromString(str);
