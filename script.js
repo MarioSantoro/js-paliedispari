6fd75a07aacb3e4c0008258db7826a9e020cdcf7
MarioSantoro <mariosantoro124@gmail.com>
2023-05-22 16:07:39 +0200

    Fix the entire code of checking palindrom string

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function checkPalindromString(words) { 
-    let len = words.length;
-    
-    for (let i = 0; i < 1; i++) {  
-        if (words[i] !== words[len - 1 ]) {  
-           console.log("La parola inserita non è palindroma");
-        }else{
-            console.log("La parola inserita è palindroma");
-        }
-    }
+   let reverseString = "";
+  for(let i = words.length-1; i >= 0; i--){
+    reverseString += words[i];
+  }
+
+  if(words === reverseString){
+    console.log("La parola è palindroma");
+  }else{
+    console.log("La parola non è palindroma");
+  }
 }  
 
 
 let str = prompt("Inserisci una parola che sia palindroma");
-
 checkPalindromString(str);
